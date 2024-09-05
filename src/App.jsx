dd5eb7f908ab7716fd4a52d2c49623b8b68e1517
Serhii Bublyk <serhiibublyk88@gmail.com>
2024-09-05 19:46:26 +0200

    setup a project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import styled from 'styled-components'
+
+const Div = styled.div`
+      text-align: center;
+      `;
+
+function App() {
+ 
+
+  return (
+    
+      <Div>
+        <i className="fa fa-calendar"></i>
+        
+        <div>123</div>
+      </Div>
+    
+  );
+}
+
+export default App
