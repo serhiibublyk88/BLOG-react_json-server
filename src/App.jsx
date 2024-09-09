588adf3aa2a07f61a0cc7639540d713a0c32a0b7
Serhii Bublyk <serhiibublyk88@gmail.com>
2024-09-09 17:58:33 +0200

    implement the main component with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
+import{Routes,Route} from 'react-router-dom';
+import styled from "styled-components";
 
-import styled from 'styled-components'
+const Content = styled.div`
+  padding:120px 0;
+`
+  const H2 = styled.h2`
+  text-align: center;
 
-const Div = styled.div`
-      text-align: center;
-      `;
+`;
+const Header =()=> <div> Header</div>
+const Footer = () => <div> Footer</div>;
 
 function App() {
- 
-
   return (
-    
-      <Div>
-        <i className="fa fa-calendar"></i>
-        
-        <div>123</div>
-      </Div>
-    
+    <>
+      <Header></Header>
+      <Content>
+        <H2> Content</H2>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/login" element={<div>Login</div>} />
+          <Route path="/register" element={<div>Registration</div>} />
+          <Route path="/users" element={<div>Users</div>} />
+          <Route path="/post/" element={<div>New Post</div>} />
+          <Route path="/post/:postId" element={<div>Post</div>} />
+          <Route path="*" element={<div>Error</div>} />
+        </Routes>
+      </Content>
+      <Footer></Footer>
+    </>
   );
 }
 
-export default App
+export default App;
